5a1019d791f2f38b88ca54f6f8a15cada118ff67
mcshean <mcshean@011-31.bxsci.78x445.nycboe.org>
2016-01-15 17:35:59 -0500

    Merge

--- src/org/usfirst/frc/team1155/robot/subsystems/UltrasonicSubsystem.java
+++ src/org/usfirst/frc/team1155/robot/subsystems/UltrasonicSubsystem.java
@@ -8,7 +8,8 @@ public class UltrasonicSubsystem {
 	Ultrasonic leftUltrasonic, rightUltrasonic;
 	
 	//In feet
-	private static final double closestDistance = 12;
+	private static final double CLOSEST_DISTANCE = 12;
+	private static final double DISTANCE_BETWEEN_ULTRASONICS = 12;
 	
 	public UltrasonicSubsystem() {
 		leftUltrasonic = Hardware.INSTANCE.leftUltrasonic;
@@ -23,13 +24,38 @@ public class UltrasonicSubsystem {
 	}
 	
 	public boolean isPathObstructed() {
-		if(leftUltrasonic.getRangeInches() <= closestDistance && rightUltrasonic.getRangeInches() <= closestDistance)
+		if(leftUltrasonic.getRangeInches() <= CLOSEST_DISTANCE && rightUltrasonic.getRangeInches() <= CLOSEST_DISTANCE)
 			return true;
 		else 
 			return false;
 	}
 	
+	public double findClosestRange() {
+		double range;
+		if (leftUltrasonic.getRangeInches() < rightUltrasonic.getRangeInches()) {
+    		range = leftUltrasonic.getRangeInches();
+    	} else {
+    		range = rightUltrasonic.getRangeInches();
+    	}
+		return range;
+	}
+	
 	public double angleToAlignTo() {
+		//negative is left, positive is right
 		
+		//find distance from both ultrasonics
+		double leftDistance = leftUltrasonic.getRangeInches();
+		double rightDistance = rightUltrasonic.getRangeInches();
+		//get the angle
+		if (rightDistance - leftDistance > 0){
+			double e = Math.sqrt(Math.pow(DISTANCE_BETWEEN_ULTRASONICS, 2) + Math.pow((rightDistance-leftDistance) , 2));
+			double f = Math.sqrt(Math.pow(DISTANCE_BETWEEN_ULTRASONICS, 2) + Math.pow(rightDistance, 2));
+			return Math.acos(Math.pow(leftDistance, 2) - Math.pow(e, 2) - Math.pow(f, 2) + 2 * e * f);
+		} else if (rightDistance - leftDistance < 0) {
+			//needs to be implemented; 
+		} else {
+			return 0;
+		}
+		return 0;
 	}
 }

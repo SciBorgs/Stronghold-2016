0e3872e2d31ecc71e5a06bb76899242b4cde465a
yez <yez@011-31.bxsci.78x445.nycboe.org>
2016-01-22 17:34:05 -0500

    Renamed

--- src/org/usfirst/frc/team1155/robot/commands/AutoDrive.java
+++ src/org/usfirst/frc/team1155/robot/commands/AutoDrive.java
@@ -6,14 +6,14 @@ import org.usfirst.frc.team1155.robot.subsystems.Drive;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
-public class EncoderDrive extends Command {
+public class AutoDrive extends Command {
 	private static Drive drive = Robot.drive;
 	private static double distanceToDrive; // in feet, distance from midline to
 											// past outerworks
 	private static boolean isFinished;
 
 	private static double driveSpeed = 1/(distanceToDrive - drive.getDistanceDriven()); //speed to not overshoot
-	public EncoderDrive(double distance) {
+	public AutoDrive(double distance) {
 		requires(Robot.drive);
 		distanceToDrive = distance;
 	}

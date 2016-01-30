a553170dd0509a2fecb59b48fda73e3c73ff8a66
SciBorgs <sternm@bxscience.edu>
2016-01-30 16:35:54 -0500

    Should of pushed before IMAGE

--- src/org/usfirst/frc/team1155/robot/subsystems/ImageSubsystem.java
+++ src/org/usfirst/frc/team1155/robot/subsystems/ImageSubsystem.java
@@ -17,8 +17,12 @@ public class ImageSubsystem extends Subsystem {
 	private int session;
 	private int numParticles;
 	private Vector<Report> particles;
-	private static final double TARGET_W_M = .40, TARGET_H_M = .13, FOV_VERT_ANGLE = 0, FOV_HORZ_ANGLE = 0,
-			FOV_H_PIXEL = 1280, FOV_W_PIXEL = 720;
+	private static final double TARGET_W_M = .40, 
+								TARGET_H_M = .13, 
+								FOV_VERT_ANGLE_RADIAN = 1.196, 
+								FOV_HORZ_ANGLE_RADIAN = 1.196,
+								FOV_W_PIXEL = 1280, 
+								FOV_H_PIXEL = 720;
 	private static final Range TAPE_HUE_RANGE = new Range(100, 175);
 	private static final Range TAPE_SAT_RANGE = new Range(100, 200);
 	private static final Range TAPE_LUM_RANGE = new Range(100, 255);
@@ -35,11 +39,13 @@ public class ImageSubsystem extends Subsystem {
 
 	public void recordVideo() {
 		NIVision.IMAQdxGrab(session, targetFrame, 1);
-		CameraServer.getInstance().setImage(targetFrame);
+		//CameraServer.getInstance().setImage(targetFrame);
 	}
 
 	public void takePicture() {
 		NIVision.imaqColorThreshold(targetImage, targetFrame, 255, NIVision.ColorMode.HSL, TAPE_HUE_RANGE, TAPE_SAT_RANGE, TAPE_LUM_RANGE);
+		NIVision.imaqFlatten(targetImage, NIVision.FlattenType.FLATTEN_IMAGE, NIVision.CompressionType.COMPRESSION_NONE, 100);
+		CameraServer.getInstance().setImage(targetImage);
 	}
 
 	public boolean doesTargetExist() {
@@ -52,23 +58,13 @@ public class ImageSubsystem extends Subsystem {
 		}
 	}
 
-	/*
-	 * public boolean checkPictureColor(int redLow, int redHigh, int greenLow,
-	 * int greenHigh, int blueLow, int blueHigh) throws NIVisionException{
-	 * hsl_target = (HSLImage) target_image; BinaryImage test =
-	 * hsl_target.thresholdRGB(redLow, redHigh, greenLow, greenHigh, blueLow,
-	 * blueHigh); if (is_image_stored) { return
-	 * NIVision.imaqMeasureParticle(test.image, index, 0,
-	 * NIVision.MeasurementType.MT_AREA) > 0; } else { return false; } }
-	 */
-
 	// return (x_distance, x_angle) & (y_distance, y_angle)
 	public void analyzeImage() {
 		for (int p = 0; p < numParticles; p++) {
 			Report report = new Report();
 			report.area = NIVision.imaqMeasureParticle(targetImage, p, 0, NIVision.MeasurementType.MT_AREA_BY_IMAGE_AREA);
-			report.width = NIVision.imaqMeasureParticle(targetImage, p, 0, NIVision.MeasurementType.MT_BOUNDING_RECT_WIDTH);
-			report.height = NIVision.imaqMeasureParticle(targetImage, p, 0, NIVision.MeasurementType.MT_BOUNDING_RECT_HEIGHT);
+			report.width = NIVision.imaqMeasureParticle(targetImage, p, 0, NIVision.MeasurementType.MT_AVERAGE_HORIZ_SEGMENT_LENGTH);
+			report.height = NIVision.imaqMeasureParticle(targetImage, p, 0, NIVision.MeasurementType.MT_AVERAGE_VERT_SEGMENT_LENGTH);
 			report.targetX = NIVision.imaqMeasureParticle(targetImage, p, 0, NIVision.MeasurementType.MT_CENTER_OF_MASS_X);
 			report.targetY = NIVision.imaqMeasureParticle(targetImage, p, 0, NIVision.MeasurementType.MT_CENTER_OF_MASS_X);
 			particles.add(report);
@@ -78,26 +74,17 @@ public class ImageSubsystem extends Subsystem {
 	
 	public TargetVector getTargetVector() {
 		// Assumes that only one object is present in image
-		double targetWidthPixels = particles.get(0).width;
-		System.out.println(targetWidthPixels);
-		double targetHeightPixels =  particles.get(0).height;
-		double targetPositionX = particles.get(0).targetX;
-		double targetPositionY =  particles.get(0).targetY;
+		double targetWidthPixels = -(particles.get(0).width - FOV_W_PIXEL/2)/(FOV_W_PIXEL/2);
+		double targetHeightPixels =  -(particles.get(0).height - FOV_H_PIXEL/2)/(FOV_H_PIXEL/2);
+		double targetPositionX = -(particles.get(0).targetX - FOV_W_PIXEL/2)/(FOV_W_PIXEL/2);
+		double targetPositionY =  -(particles.get(0).targetY - FOV_H_PIXEL/2)/(FOV_H_PIXEL/2);
 
-		// OR
-		// Use your imagination and allow more objects :P
-		// double aiming_x = (target_position_x -
-		// IMAGE_RESOLUTION_X/2)/(IMAGE_RESOLUTION_X/2);
-		// double aiming_y = (target_position_y -
-		// IMAGE_RESOLUTION_Y/2)/(IMAGE_RESOLUTION_Y/2);
-		// Get theta (on x-axis) and phi (on y-axis)
-
-		double theta = (targetWidthPixels - targetPositionX) / (targetWidthPixels) * FOV_HORZ_ANGLE;
-		double phi = (targetHeightPixels - targetPositionY) / (targetHeightPixels) * FOV_VERT_ANGLE;
+		double theta = (targetWidthPixels - targetPositionX) / (targetWidthPixels) * FOV_HORZ_ANGLE_RADIAN;
+		double phi = (targetHeightPixels - targetPositionY) / (targetHeightPixels) * FOV_VERT_ANGLE_RADIAN;
 
 		// formula for distance
-		double distanceX = TARGET_W_M * FOV_W_PIXEL / (2 * targetWidthPixels * Math.tan(theta));
-		double distanceY = TARGET_H_M * FOV_H_PIXEL / (2 * targetHeightPixels * Math.tan(phi));
+		double distanceX = TARGET_W_M * FOV_W_PIXEL / (2 * targetWidthPixels * Math.tan(FOV_HORZ_ANGLE_RADIAN));
+		double distanceY = TARGET_H_M * FOV_H_PIXEL / (2 * targetHeightPixels * Math.tan(FOV_VERT_ANGLE_RADIAN));
 
 		// IGNORE dist_y and phi for limbot
 		TargetVector targetVector = new TargetVector();

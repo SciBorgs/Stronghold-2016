2b480701cff7688b81d0088ec07992d37c582ac5
sciborgs <sciborgs@bxscience.edu>
2016-03-18 15:42:46 -0400

    Added code to the ClimbSubsystem

--- src/org/usfirst/frc/team1155/robot/subsystems/ClimbSubsystem.java
+++ src/org/usfirst/frc/team1155/robot/subsystems/ClimbSubsystem.java
@@ -12,10 +12,37 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class ClimbSubsystem extends Subsystem {
        
 	public CANTalon winchTalon, armTalon;
-	
-    public void initDefaultCommand() {
+    
+    public ClimbSubsystem() {
         winchTalon = new CANTalon(PortMap.CLIMB_WINCH_TALON);
+        winchTalon.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
+        
     	armTalon = new CANTalon(PortMap.CLIMB_ARM_TALON);
+    	armTalon.changeControlMode(CANTalon.TalonControlMode.Position);
     }
+    
+    public boolean openArm() {
+    	armTalon.set(1023);
+    	return true;
+    }
+    
+    public boolean closeArm() {
+    	armTalon.set(0);
+    	return true;
+    }
+    
+    public void extendWinch() {
+    	winchTalon.set(0.5);
+    }
+    
+    public void retractWinch() {
+    	winchTalon.set(-0.5);
+    }
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
 }
 
